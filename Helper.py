03f53c309aaedf23222d7dc8ff2435f00d3bf410
Bar Dadon <bdadon50@gmail.com>
2022-11-29 01:19:03 +0200

    v1

--- Helper.py
+++ Helper.py
@@ -1,4 +1,9 @@
 def print_class_methods(b1):
 
-    method_list = [method for method in dir(b1) if '__' not in method]
+    method_list = []
+    for method in dir(b1):
+        if '__' not in method:
+            method_list.append(method)
+
     return method_list
+
