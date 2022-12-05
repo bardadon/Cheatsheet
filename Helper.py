7c873165b7c89fc49585000d64ea04e4309acb16
Bar Dadon <bdadon50@gmail.com>
2022-12-06 00:18:48 +0200

    v1

--- Helper.py
+++ Helper.py
@@ -7,3 +7,27 @@ def print_class_methods(b1):
 
     return method_list
 
+def pipFreeze_RemoveVersion(temp_file, requirements_file = 'requirements.txt'):
+    
+    '''
+    Remove version requirements from pip freeze.
+    Create a file using pip freeze before running this function.
+    ------------------Pip freeze >> temp.txt------------------
+
+    Args:
+        - temp_file = text file
+    Returns:
+        - None
+    '''
+
+    with open(temp_file, 'r') as read_file:
+
+        with open(requirements_file, 'w') as write_file:
+
+            for line in read_file:
+
+                start_location = line.find('=')
+                line = line[0:start_location] + '\n'
+                
+                write_file.writelines(line)
+
